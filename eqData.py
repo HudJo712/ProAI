96f77604cdbf40fd2f58ed2dc9c5c07a9d68bd63
HudJo712 <[183376121+HudJo712@users.noreply.github.com]>
2024-10-07 20:04:31 +0200

    Your commit message here

---
+++ eqData.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import streamlit as st
+
+title1="Global Earthquake Data"
+st.title(title1)
+st.write("Comprehensive dataset of global earthquakes with key attributes for analysis: https://www.kaggle.com/datasets/shreyasur965/recent-earthquakes")
+eq_data=pd.read_csv("/home/hudjo712/ProAI/earthquakes.csv")
+
+title2="Magnitude"
+st.title(title2)
+st.write(f'Min:{eq_data['magnitude'].min()}')
+st.write(f'Mean:{eq_data['magnitude'].mean()}')
+st.write(f'Max:{eq_data['magnitude'].max()}')
+
+title3="Network"
+st.title(title3)
+network_list=eq_data['net'].unique()
+eq_numbers=eq_data['id'].value_counts().reset_index()
+st.write("Network")
+selected_Network=st.selectbox('Network',network_list)
+df_network=eq_data[eq_data['net']==selected_Network]
+st.write(f'The number of earthquakes for {selected_Network}:{df_network['id'].count}')
+
+title4="Number of earthquakes by network"
+st.title(title4)
+eq_numbers.columns=['title','id']
+st.bar_chart(eq_numbers.set_index('title'))
